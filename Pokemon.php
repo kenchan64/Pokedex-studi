88659db9cde46a6e1b1a5f8911c2bda1de2ca554
kenchan64 <117024099+kenchan64@users.noreply.github.com>
2023-03-04 17:23:22 +0100

    add Model & Controller

---
+++ Pokemon.php
@@ -0,0 +1,122 @@
+<?php
+
+class Pokemon
+{
+    private $number;
+    private $name;
+    private $description;
+    private $type1;
+    private $type2;
+    private $id;
+
+    public function __construct(array $data) {
+        $this->hydrate($data);
+    }
+
+    public function hydrate(array $data)
+    {
+        foreach ($data as $key => $value) {
+            $method = "set" . ucfirst($key);
+            if ($method_exists($this, $method)) {
+                $this->$method($value);
+            }
+        }
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param mixed $id
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getNumber()
+    {
+        return $this->number;
+    }
+
+    /**
+     * @param mixed $number
+     */
+    public function setNumber($number)
+    {
+        $this->number = $number;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param mixed $name
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getDescription()
+    {
+        return $this->description;
+    }
+
+    /**
+     * @param mixed $description
+     */
+    public function setDescription($description)
+    {
+        $this->description = $description;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getType1()
+    {
+        return $this->type1;
+    }
+
+    /**
+     * @param mixed $type1
+     */
+    public function setType1($type1)
+    {
+        $this->type1 = $type1;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getType2()
+    {
+        return $this->type2;
+    }
+
+    /**
+     * @param mixed $type2
+     */
+    public function setType2($type2)
+    {
+        $this->type2 = $type2;
+    }
+
+}
